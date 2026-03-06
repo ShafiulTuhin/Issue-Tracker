878f5860a1700391b975f1eb24e6116e39da84e7
ShafiulTuhin <tuhin6488@gmail.com>
2026-03-07 02:09:58 +0600

    Complete modal

--- script.js
+++ script.js
@@ -16,16 +16,27 @@ loginBtn &&
       window.location.assign("home.html");
     }
   });
-
+// Manage Spinner
+const manageSpinner = (status) => {
+  if (status === true) {
+    document.getElementById("spinner").classList.remove("hidden");
+    document.getElementById("issue-container").classList.add("hidden");
+  } else {
+    document.getElementById("spinner").classList.add("hidden");
+    document.getElementById("issue-container").classList.remove("hidden");
+  }
+};
 //   Get all Issues
 let allIssues = [];
 const loadAllIssues = async () => {
+  manageSpinner(true);
   const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
   const res = await fetch(url);
   const data = await res.json();
   allIssues = data.data;
-  displayIssues(allIssues);
+  displayAllIssues(allIssues);
   countAllIssues();
+  manageSpinner(false);
 };
 
 // Render Issues
@@ -51,13 +62,13 @@ const displayIssues = (issues) => {
     const renderIssue = document.createElement("div");
 
     renderIssue.innerHTML = `
-  <div class="bg-white p-4 space-y-4 rounded-lg ${borderTop}">
+  <div onclick="displayModal('${issue.id}')" class="bg-white p-4 space-y-4 rounded-lg ${borderTop}">
           <div class="flex justify-between items-center">
             <img src="${priorityImage}" alt="" />
-            <h2 class="px-4 ${priorityColor} ">${issue.priority}</h2>
+            <h2 class="px-4 ${priorityColor} ">${issue.priority ? issue.priority : "Not defined"}</h2>
           </div>
           <h2 class="text-[#1F2937] font-semibold">
-         ${issue.title}
+         ${issue.title ? issue.title : "Not defined"}
           </h2>
           <p class="text-[#64748B] line-clamp-2">
             ${issue.description}
@@ -99,7 +110,6 @@ const removeActive = () => {
 const displayAllIssues = () => {
   removeActive();
   addActive("button-all");
-
   displayIssues(allIssues);
 };
 // filter issues for open button
@@ -131,13 +141,69 @@ const countClosedIssues = () => {
   document.getElementById("issue-counter").textContent = closeCount;
 };
 
-// Get single issue
-// const getIssue = async (id) => {
-//   const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
-//   const res = await fetch(url);
-//   const data = await res.json();
-//   console.log(data);
-// };
-// getIssue();
-displayAllIssues();
+//issue details modal
+const modal = document.getElementById("issue-details-modal");
+const displayModal = async (id) => {
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data.data);
+
+  getIssueDetails(data.data);
+  modal.showModal();
+};
+
+const getIssueDetails = (issue) => {
+  const statusColor = issue.status === "open" ? "bg-green-600" : "bg-red-600";
+  const priorityBg =
+    issue.priority === "high"
+      ? "bg-red-300 text-red-600"
+      : issue.priority === "medium"
+        ? "bg-amber-100 text-amber-400"
+        : "bg-purple-200 text-purple-600";
+
+  const modalDetails = document.getElementById("modal-details");
+
+  modalDetails.innerHTML = `
+   <h2 class="font-bold text-2xl text-[#1F2937] mb-4">
+              ${issue.title}
+            </h2>
+            <div class="flex gap-4 my-6">
+              <p class="${statusColor} py-2 text-white font-medium rounded-full px-5">
+                ${issue.status === "open" ? "Opened" : "Closed"}
+              </p>
+              <p class="text-[#64748B]">Opened by ${issue.assignee}</p>
+              <p class="text-[#64748B]">${issue.createdAt}</p>
+            </div>
+            <div class="flex gap-4 shadow-sm pb-8">
+              <div
+                class="flex gap-1 items-center bg-red-100 px-4 py-1 rounded-full"
+              >
+                <img src="assets/bug.png" class="w-3 h-4" alt="" />
+                <p class="text-red-400 text-[12px]">BUG</p>
+              </div>
+              <div
+                class="flex gap-1 items-center bg-amber-200 px-4 py-1 rounded-full"
+              >
+                <img src="assets/Help.png" class="w-3 h-4" alt="" />
+                <p class="text-amber-500 text-[12px]">HELP WANTED</p>
+              </div>
+            </div>
+            <p class="text-[#64748B] mb-7">
+             ${issue.description}
+            </p>
+            <div class="flex justify-between items-center">
+              <div>
+                <p class="text-[#64748B]">Assignee:</p>
+                <p class="text-[#1F2937] font-bold">${issue.assignee ? issue.assignee : "Not Mentioned"}</p>
+              </div>
+              <div class="">
+                <p class="text-[#64748B]">Priority:</p>
+                <p class="${priorityBg}  px-3 rounded-full">${issue.priority}</p>
+              </div>
+            </div>
+
+`;
+};
+
 loadAllIssues();
